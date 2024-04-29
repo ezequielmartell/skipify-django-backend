d193abf99a23d144e0f4cb7c8abcfd3dc897bd81
ezequielmartell <ezequiel@thinkmartell.com>
2024-04-29 02:33:03 +0000

    add postgresql, and environ

--- django_aws/settings.py
+++ django_aws/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env_path = BASE_DIR / ".env"
+if env_path.is_file():
+    environ.Env.read_env(env_file=str(env_path))
 
 
 # Quick-start development settings - unsuitable for production
@@ -73,12 +79,17 @@ WSGI_APPLICATION = 'django_aws.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(default="postgresql://postgres:postgres@127.0.0.1:5433/django_aws")
 }
+
 
 
 # Password validation
